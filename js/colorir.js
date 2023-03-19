02e48410c95f6c3b47a6dcb9fa4a0e75271eef5e
Adriano Batista <batdavid70@gmail.com>
2023-03-18 23:52:56 -0400

    ListaDeTarefas

---
+++ js/colorir.js
@@ -0,0 +1,58 @@
+function containerCores(classe){
+    var divCores = document.createElement('div')
+    // divCores.addEventListener('click', function(){
+        
+    //     if(divCores.classList.contains('colorir')){
+    //         divCores.classList.remove('colorir')
+    //     }else{
+    //         divCores.classList.add('colorir')
+    //     }
+    // })
+
+    divCores.classList.add(classe)
+    // divCores.appendChild(adicionaCores('fa-solid', 'fa-caret-up', 'cima'))
+    divCores.appendChild(adicionaCores('dimencoes', 'normal'))
+    divCores.appendChild(adicionaCores('dimencoes', 'laranja'))
+    divCores.appendChild(adicionaCores('dimencoes', 'verde'))
+    // divCores.appendChild(adicionaCores( 'fa-solid', 'fa-palette', 'branco'))
+   
+    
+
+    
+
+
+    return divCores
+}
+
+function adicionaCores(classe, classe1 ,classe2){
+    var cores = document.createElement('i')
+    cores.classList.add(classe, classe1, classe2)
+    return cores
+}
+
+
+var ulTodas = document.querySelector('#ol')
+ulTodas.addEventListener('click', function(e){
+    var mudar = e.target;
+    var laranja = mudar.classList.contains('laranja');
+    var normal = mudar.classList.contains('normal');
+    var verde = mudar.classList.contains('verde');
+    if(laranja){        
+        mudar.parentNode.parentNode.parentNode.classList.add('orange')
+        mudar.parentNode.parentNode.parentNode.classList
+        .remove('color','green')
+        console.log(mudar.parentNode.parentNode.parentNode)
+    } else if(normal){        
+        mudar.parentNode.parentNode.parentNode.classList.add('color')
+        mudar.parentNode.parentNode.parentNode.classList
+        .remove('orange','green')
+        console.log(mudar.parentNode.parentNode.parentNode)
+    } else if(verde){        
+        mudar.parentNode.parentNode.parentNode.classList.add('green')
+        mudar.parentNode.parentNode.parentNode.classList
+        .remove('color','orange')
+        console.log(mudar.parentNode.parentNode.parentNode)
+    }
+
+    
+})
